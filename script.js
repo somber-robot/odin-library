696b233596c4d944171b9fee8a62354fb5b5c717
somber-robot <somberrobot@gmail.com>
2026-07-05 20:30:26 +0100

    card version

--- script.js
+++ script.js
@@ -7,61 +7,51 @@ function Book(title, author, pageCount, read){
     this.author = author;
     this.pages = pageCount;
     this.read = read;
-
-    this.info = () => {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
-    };
 }
 
 function addBookToLibrary(title, author, pageCount) {
-    myLibrary.push(new Book(title, author, pageCount, false));
+    let book = new Book(title, author, pageCount, false)
+    myLibrary.push(book);
+    createCard(book);
 }
 
-const table = document.querySelector("#books");
+let addBtn = document.querySelector("#add-book");
 
-let index = 1;
-
-function displayBooks(){
-    myLibrary.forEach(function(book, i){
-        if (i+1 < index) return;
-        let row = document.createElement("tr");
-        let sn = document.createElement("td");
-        sn.innerText = `${index++}`;
-        row.appendChild(sn);
-        let title = document.createElement("td");
-        title.innerText = book.title;
-        row.appendChild(title);
-        let author = document.createElement("td");
-        author.innerText = book.author;
-        row.appendChild(author);
-        let pages = document.createElement("td");
-        pages.innerText = book.pages;
-        row.appendChild(pages);
-        let status = document.createElement("td");
-        status.innerText = book.read ? "Read" : "Not Read";
-        row.appendChild(status);
-        table.appendChild(row);
-    });
-}
-
-const dialog = document.querySelector("#form");
-const submit = document.querySelector("#submit");
-submit.addEventListener("click", function(e){
-    e.preventDefault();
-    let titleInput = document.querySelector("#title")
-    let title = titleInput.value;
-    titleInput.value = "";
-    let authorInput = document.querySelector("#author");
-    let author = authorInput.value;
-    authorInput.value = "";
-    let pagesInput = document.querySelector("#pages");
-    let pages = pagesInput.value;
-    pagesInput.value = "";
-    if (title === "" | author === "" | pages === ""){
-        alert("fill all data fields");
-        return;
-    }
-    addBookToLibrary(title, author, pages);
-    displayBooks();
-    dialog.close();
+addBtn.addEventListener("click", function(e){
+    addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 256);
 });
+
+function createCard(book){
+    let card = document.createElement("div");
+    card.classList.add("card");
+    if(book.read) card.classList.add("card-read");
+    let title = document.createElement("p");
+    title.innerText = book.title;
+    title.classList.add("title");
+    let author = document.createElement("p");
+    author.innerText = `By: ${book.author}`;
+    author.classList.add("author");
+    let pages = document.createElement("p");
+    pages.innerText = `${book.pages} pages`;
+    pages.classList.add("pages");
+    let status = document.createElement("button");
+    status.innerText = "Toggle Status";
+    status.classList.add("status"); 
+    status.addEventListener("click", function(e){
+        if (!card.classList.contains("card-read")){
+            card.classList.add("card-read");
+        }else{
+            card.classList.remove("card-read");
+        }
+    });
+    let remove = document.createElement("button");
+    remove.innerText = "Remove Book";
+    remove.classList.add("delete");
+    remove.addEventListener("click", function(e){
+        myLibrary.splice(myLibrary.indexOf(book), 1);
+        card.parentNode.removeChild(card);
+    });
+    card.append(title, author, pages, status, remove);
+    let cards = document.querySelector("#cards");
+    cards.insertBefore(card, addBtn);
+}   
